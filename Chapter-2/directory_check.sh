c7d8966b33db08d49222b06cfce852297f904275
rdkerr <rogerdkerr@gmail.com>
2018-05-10 13:34:41 -0400

    Removed duplicates

--- Chapter-2/directory_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /
-
-for d in ./*/ ; do (cd "$d" && for d in ./*/ ; do (cd "$d" && pwd && diff -U 0 <(ls ./) <(ls ../) | grep -c ^@); done ); done
-
-pwd
-diff -U 0 <(ls ./) <(ls ../) | grep -c ^@
--- lib/error.c
+++ lib/error.c
@@ -127,4 +127,3 @@ err_doit(int errnoflag, int error, const char *fmt, va_list ap)
 	fputs(buf, stderr);
 	fflush(NULL);		/* flushes all stdio output streams */
 }
-for d in ./*/ ; do (cd "$d" && for d in ./*/ ; do (cd "$d" && ls); done ); done
